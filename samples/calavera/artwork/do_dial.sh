328660315992d405d0a3abfa5078dcaa576abf91
Peter Avdjian <pavdjian@paperlesspost.com>
2021-01-30 09:10:29 +0000

    Copied Analogue directory to Calavera
    Need to clean this up but should be enough to stash changes

---
+++ samples/calavera/artwork/do_dial.sh
@@ -0,0 +1,111 @@
+#########################
+#                       #
+#       390 x 390       #
+#                       #
+#########################
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/markers.png -o samples/analogue/resources-round-390x390/markers -r 390
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-390x390/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 390
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-390x390/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 390
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-390x390/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 390
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-390x390/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 390
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-390x390/hour-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 390
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-390x390/hour-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 390
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-390x390/minute-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 390
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-390x390/minute-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 390
+
+
+#########################
+#                       #
+#       240 x 240       #
+#                       #
+#########################
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/markers.png -o samples/analogue/resources-round-240x240/markers -r 240
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-240x240/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 240
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-240x240/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 240
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-240x240/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 240
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-240x240/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 240
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-240x240/hour-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 240
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-240x240/hour-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 240
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-240x240/minute-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 240
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-240x240/minute-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 240
+
+
+
+#########################
+#                       #
+#       280 x 280       #
+#                       #
+#########################
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/markers.png -o samples/analogue/resources-round-280x280/markers -r 280
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-280x280/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-280x280/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-280x280/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-280x280/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-280x280/hour-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-280x280/hour-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-280x280/minute-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-280x280/minute-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
+
+
+# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-dial-* -o samples/analogue/resources-round-280x280/dial -r 280
+# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-hour* -o samples/analogue/resources-round-280x280/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
+# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-hour* -o samples/analogue/resources-round-280x280/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
+# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-minute* -o samples/analogue/resources-round-280x280/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
+# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-minute* -o samples/analogue/resources-round-280x280/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
+
+
+#########################
+#                       #
+#       260 x 260       #
+#                       #
+#########################
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/markers.png -o samples/analogue/resources-round-260x260/markers -r 260
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-260x260/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-260x260/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-260x260/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-260x260/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-260x260/hour-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-260x260/hour-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
+
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-260x260/minute-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
+/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-260x260/minute-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
+
+# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-dial-* -o samples/analogue/resources-round-260x260/dial -r 260
+# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-minute* -o samples/analogue/resources-round-260x260/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
+# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-minute* -o samples/analogue/resources-round-260x260/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
+# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-hour* -o samples/analogue/resources-round-260x260/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
+# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-hour* -o samples/analogue/resources-round-260x260/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
+
+
+
+#########################
+#                       #
+#       218 x 218       #
+#                       #
+#########################
+
+
+# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-dial-* -o samples/analogue/resources-round-218x218/dial -r 218
+# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-hour* -o samples/analogue/resources-round-218x218/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 218
+# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-hour* -o samples/analogue/resources-round-218x218/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 218
+# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-minute* -o samples/analogue/resources-round-218x218/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 218
+# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-minute* -o samples/analogue/resources-round-218x218/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 218
