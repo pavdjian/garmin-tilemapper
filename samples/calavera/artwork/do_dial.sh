6f29555d1f5518761311da6d34c4897dee2d9378
Peter Avdjian <pavdjian@paperlesspost.com>
2021-01-30 09:11:59 +0000

    Rename analogue to calavera in do_dial.sh

--- samples/calavera/artwork/do_dial.sh
+++ samples/calavera/artwork/do_dial.sh
@@ -4,19 +4,19 @@
 #                       #
 #########################
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/markers.png -o samples/analogue/resources-round-390x390/markers -r 390
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/markers.png -o samples/calavera/resources-round-390x390/markers -r 390
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-390x390/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 390
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-390x390/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 390
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour.png -o samples/calavera/resources-round-390x390/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 390
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour.png -o samples/calavera/resources-round-390x390/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 390
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-390x390/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 390
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-390x390/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 390
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute.png -o samples/calavera/resources-round-390x390/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 390
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute.png -o samples/calavera/resources-round-390x390/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 390
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-390x390/hour-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 390
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-390x390/hour-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 390
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour-overlay.png -o samples/calavera/resources-round-390x390/hour-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 390
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour-overlay.png -o samples/calavera/resources-round-390x390/hour-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 390
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-390x390/minute-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 390
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-390x390/minute-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 390
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute-overlay.png -o samples/calavera/resources-round-390x390/minute-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 390
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute-overlay.png -o samples/calavera/resources-round-390x390/minute-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 390
 
 
 #########################
@@ -25,19 +25,19 @@
 #                       #
 #########################
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/markers.png -o samples/analogue/resources-round-240x240/markers -r 240
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/markers.png -o samples/calavera/resources-round-240x240/markers -r 240
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-240x240/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 240
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-240x240/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 240
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour.png -o samples/calavera/resources-round-240x240/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 240
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour.png -o samples/calavera/resources-round-240x240/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 240
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-240x240/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 240
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-240x240/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 240
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute.png -o samples/calavera/resources-round-240x240/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 240
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute.png -o samples/calavera/resources-round-240x240/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 240
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-240x240/hour-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 240
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-240x240/hour-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 240
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour-overlay.png -o samples/calavera/resources-round-240x240/hour-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 240
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour-overlay.png -o samples/calavera/resources-round-240x240/hour-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 240
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-240x240/minute-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 240
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-240x240/minute-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 240
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute-overlay.png -o samples/calavera/resources-round-240x240/minute-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 240
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute-overlay.png -o samples/calavera/resources-round-240x240/minute-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 240
 
 
 
@@ -47,26 +47,26 @@
 #                       #
 #########################
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/markers.png -o samples/analogue/resources-round-280x280/markers -r 280
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/markers.png -o samples/calavera/resources-round-280x280/markers -r 280
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-280x280/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-280x280/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour.png -o samples/calavera/resources-round-280x280/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour.png -o samples/calavera/resources-round-280x280/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-280x280/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-280x280/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute.png -o samples/calavera/resources-round-280x280/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute.png -o samples/calavera/resources-round-280x280/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-280x280/hour-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-280x280/hour-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour-overlay.png -o samples/calavera/resources-round-280x280/hour-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour-overlay.png -o samples/calavera/resources-round-280x280/hour-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-280x280/minute-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-280x280/minute-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute-overlay.png -o samples/calavera/resources-round-280x280/minute-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute-overlay.png -o samples/calavera/resources-round-280x280/minute-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
 
 
-# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-dial-* -o samples/analogue/resources-round-280x280/dial -r 280
-# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-hour* -o samples/analogue/resources-round-280x280/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
-# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-hour* -o samples/analogue/resources-round-280x280/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
-# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-minute* -o samples/analogue/resources-round-280x280/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
-# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-minute* -o samples/analogue/resources-round-280x280/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
+# /usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/calavera-dial-* -o samples/calavera/resources-round-280x280/dial -r 280
+# /usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/calavera-hour* -o samples/calavera/resources-round-280x280/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
+# /usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/calavera-hour* -o samples/calavera/resources-round-280x280/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
+# /usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/calavera-minute* -o samples/calavera/resources-round-280x280/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 280
+# /usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/calavera-minute* -o samples/calavera/resources-round-280x280/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 280
 
 
 #########################
@@ -75,25 +75,25 @@
 #                       #
 #########################
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/markers.png -o samples/analogue/resources-round-260x260/markers -r 260
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/markers.png -o samples/calavera/resources-round-260x260/markers -r 260
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-260x260/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour.png -o samples/analogue/resources-round-260x260/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour.png -o samples/calavera/resources-round-260x260/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour.png -o samples/calavera/resources-round-260x260/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-260x260/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute.png -o samples/analogue/resources-round-260x260/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute.png -o samples/calavera/resources-round-260x260/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute.png -o samples/calavera/resources-round-260x260/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-260x260/hour-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/hour-overlay.png -o samples/analogue/resources-round-260x260/hour-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour-overlay.png -o samples/calavera/resources-round-260x260/hour-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/hour-overlay.png -o samples/calavera/resources-round-260x260/hour-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
 
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-260x260/minute-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
-/usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/minute-overlay.png -o samples/analogue/resources-round-260x260/minute-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute-overlay.png -o samples/calavera/resources-round-260x260/minute-overlay-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
+/usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/minute-overlay.png -o samples/calavera/resources-round-260x260/minute-overlay-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
 
-# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-dial-* -o samples/analogue/resources-round-260x260/dial -r 260
-# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-minute* -o samples/analogue/resources-round-260x260/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
-# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-minute* -o samples/analogue/resources-round-260x260/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
-# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-hour* -o samples/analogue/resources-round-260x260/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
-# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-hour* -o samples/analogue/resources-round-260x260/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
+# /usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/calavera-dial-* -o samples/calavera/resources-round-260x260/dial -r 260
+# /usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/calavera-minute* -o samples/calavera/resources-round-260x260/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
+# /usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/calavera-minute* -o samples/calavera/resources-round-260x260/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
+# /usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/calavera-hour* -o samples/calavera/resources-round-260x260/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 260
+# /usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/calavera-hour* -o samples/calavera/resources-round-260x260/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 260
 
 
 
@@ -104,8 +104,8 @@
 #########################
 
 
-# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-dial-* -o samples/analogue/resources-round-218x218/dial -r 218
-# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-hour* -o samples/analogue/resources-round-218x218/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 218
-# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-hour* -o samples/analogue/resources-round-218x218/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 218
-# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-minute* -o samples/analogue/resources-round-218x218/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 218
-# /usr/local/bin/python3 tilemapper.py -i samples/analogue/artwork/analogue-minute* -o samples/analogue/resources-round-218x218/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 218
+# /usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/calavera-dial-* -o samples/calavera/resources-round-218x218/dial -r 218
+# /usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/calavera-hour* -o samples/calavera/resources-round-218x218/hour-0-29 -m rotate -u 0 -v -174 -s -6 -r 218
+# /usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/calavera-hour* -o samples/calavera/resources-round-218x218/hour-30-59 -m rotate -u -180 -v -354 -s -6 -r 218
+# /usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/calavera-minute* -o samples/calavera/resources-round-218x218/minute-0-29 -m rotate -u 0 -v -174 -s -6 -r 218
+# /usr/local/bin/python3 tilemapper.py -i samples/calavera/artwork/calavera-minute* -o samples/calavera/resources-round-218x218/minute-30-59 -m rotate -u -180 -v -354 -s -6 -r 218
